42afbfb330e05b3ec85ab430a967d00f081b0fc9
alisvila <aliam1991@gmail.com>
2023-12-27 23:56:51 +0330

    some minor clean ups and overal structare feels nice

--- script.js
+++ script.js
@@ -22,6 +22,97 @@ const setYRot = gsap.quickSetter(face, "rotationY", "deg");
 
 
 
+
+// the new about
+
+
+
+select = e => document.querySelector(e);
+selectAll = e => document.querySelectorAll(e);
+const stage = select('.shapes');
+let gltl = gsap.timeline({ delay: 1 });
+// let bandST = new SplitText('.name', {type:"chars", charsClass:"bandChar", position: "absolute" }); 
+
+function intro() {
+    
+    let tl = gsap.timeline({
+        delay: 1,
+        defaults: {
+            duration: 2,
+            ease: 'power4'
+        }
+    });
+    tl.from('.names', {
+        x: function(i) {
+            if (i%2==0) {
+                return 1000;
+            }
+            return -1000;
+        },
+        stagger: 0.15    
+    })
+    .from('p span', {
+        y: 70,
+        stagger: 0.1
+    }, 2)
+    
+    return tl;
+}
+
+function loopAnim() {
+    
+    let tl = gsap.timeline({
+        repeat: -1
+    });
+    
+    tl.to('.names', {
+        y: -102,
+        duration: 5,
+        ease: 'none'
+    })
+    .from('.name__end--red .bandChar', {
+        y: 120,
+        duration: 3,
+        ease: 'power4',
+        stagger: 0.05
+    }, 1)
+    .from('.name__end--blue .bandChar', {
+        y: 120,
+        duration: 3,
+        ease: 'power4',
+        stagger: 0.05
+    }, 1.3)
+    .from('.band:nth-of-type(5) .name--blue .bandChar', {
+        y: -120,
+        duration: 2,
+        ease: 'power4.inOut',
+        stagger: -0.05
+    }, 0)
+    .to('.band:nth-of-type(6) .name--blue .bandChar', {
+        y: 120,
+        duration: 2,
+        ease: 'power4.inOut',
+        stagger: -0.05
+    }, 0)
+    
+    return tl;
+}
+
+function init() {
+    gsap.set(stage, { autoAlpha: 1 });
+    gsap.set('.content', { rotate: -25 });
+    stage.onclick = () => {
+        gltl.restart();
+    }
+    
+    gltl.add(intro());
+    gltl.add(loopAnim(), 0);
+}
+
+
+// end of new about
+
+
 let getYPercent, getXPercent;
 
 function resize() {
@@ -46,6 +137,20 @@ function resize() {
   const RF = faceArea.width/2;
   centerXF = faceArea.left + RF;
   centerYF = faceArea.top + RF;
+
+
+
+
+  let vh = window.innerHeight;
+	let sh = stage.offsetHeight;
+	let scaleFactor = vh/sh;
+	if(scaleFactor<1) {
+		gsap.set(stage, { scale: scaleFactor });
+	}
+	else {
+        gsap.set(stage, { scale: 1 });
+    }
+
 
 }
 
@@ -93,8 +198,6 @@ function updateTrans(e) {
       gsap.to(leftEyebrow, { yPercent: 1, duration: 2, overwrite: 'auto' });
       // gsap.to(face, { rotation: 2, duration: 2, overwrite: 'auto' });
   }
-
-  console.log(xF , yF)
 
   if (xF > 0 && yF > 0 ) {
     gsap.to(face, { rotation: 3, duration: 2, overwrite: 'auto' });
@@ -162,3 +265,14 @@ gsap.from(".firstAn", {
     toggleActions: "play none none reverse"
   }
 });
+
+
+
+
+window.onresize = resize;
+
+window.onload = () => {
+	init();
+    resize();
+    // GSDevTools.create();
+};
